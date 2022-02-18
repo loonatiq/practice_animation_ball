f063df64585387dab4ddb4eb644588881a567b12
loonatiq <86408147+loonatiq@users.noreply.github.com>
2022-02-19 00:27:23 +0900

    1st

--- Ball.js
+++ Ball.js
@@ -51,7 +51,7 @@ export class Ball {
       const y2 = Math.abs(this.y - maxY);
       const min1 = Math.min(x1, x2);
       const min2 = Math.min(y1, y2);
-      const min = Math.min(max1, min2);
+      const min = Math.min(min1, min2);
       if (min == min1) {
         this.vx *= -1;
         this.x += this.vx;
